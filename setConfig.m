3671e3fc58a8481ff047d296a42fe339568cfd45
Tigran Mkhoyan <tmkhoyan@wlan-145-94-214-37.wlan.tudelft.nl>
2018-04-30 14:10:55 +0200

    Final version after testing. Major improvements

--- setConfig.m
+++ setConfig.m
@@ -1,12 +1,14 @@
 function setConfig(varargin)
 
+delimiter = [];
+filename = [];
 setOptargs;
 
 cpaths = regexp(basepath,'/','split');
 
 basepath = cpaths{1};
 
-opt = readConfig(filename); %#ok<SHVAI>
+opt = readConfig(filename,'structnamefieldfillelemn', '__'); 
 
  %get_param(gcb,'DialogParameters')
 
@@ -17,20 +19,21 @@ if ischar(opt.blocknames)
     % opt.blocknames){k}{1} and opt.blocknames{k}{2}
      set_param([basepath opt.blocknames],opt.(opt.blocknames){1}{1},opt.(opt.blocknames{1}){1}{2});
 else
-    blockpaths = strcat('/',regexprep(opt.blocknames,'_','/'));
+    blockfieldnames = regexprep(opt.blocknames,'/',delimiter); %check if bloknames contain / recplace by '--' to access the struct fieldnames stored by readConfig
+    blockpaths = strcat('/',regexprep(opt.blocknames,delimiter,'/')); % default delimiter is '__'
     for k=1:numel(opt.blocknames)
-        for m = 1:numel(opt.(opt.blocknames{k}))
+        for m = 1:numel(opt.(blockfieldnames{k}))
             try
-                if strcmpi(opt.(opt.blocknames{k}){m}{2},'false')
-                    opt.(opt.blocknames{k}){m}{2} = 0;
-                elseif strcmpi(opt.(opt.blocknames{k}){m}{2},'true')
-                    opt.(opt.blocknames{k}){m}{2} = 1;
+                if strcmpi(opt.(blockfieldnames{k}){m}{2},'false')
+                    opt.(blockfieldnames{k}){m}{2} = 0;
+                elseif strcmpi(opt.(blockfieldnames{k}){m}{2},'true')
+                    opt.(blockfieldnames{k}){m}{2} = 1;
                 end
                 
-                set_param([basepath blockpaths{k}],opt.(opt.blocknames{k}){m}{1},opt.(opt.blocknames{k}){m}{2});
+                set_param([basepath blockpaths{k}],opt.(blockfieldnames{k}){m}{1},opt.(blockfieldnames{k}){m}{2});
             catch
-                %warnstr = sprintf('Parameter not set. Block ''%s'' does not have a parameter named ''%s''', blockpaths{k}, opt.(opt.blocknames{k}){m}{1});
-                warning('Parameter value ''%s'' not set. Block ''%s'' does not have a parameter named ''%s''', opt.(opt.blocknames{k}){m}{2},  blockpaths{k}, opt.(opt.blocknames{k}){m}{1});
+                %warnstr = sprintf('Parameter not set. Block ''%s'' does not have a parameter named ''%s''', blockpaths{k}, opt.(blockfieldnames{k}){m}{1});
+                warning('Parameter value ''%s'' not set. Block ''%s'' does not have a parameter named ''%s''', opt.(blockfieldnames{k}){m}{2},  blockpaths{k}, opt.(blockfieldnames{k}){m}{1});
             end
         end
     end
@@ -48,14 +51,14 @@ end
         % set defaults for optional inputs
         if numvarargs > 1
             error('functions:randRange:TooManyInputs', ...
-                'requires atmost 2 optional input');
+                'requires atmost 3 optional input');
         end
         cpaths = regexp(gcb(),'/','split'); cpaths = cpaths{1};
         
-        optargs = {'config/config.txt', cpaths};
+        optargs = {'config/config.txt', cpaths,'__'};
         %optargs{1:numvarargs} = varargin;
         [optargs{1:numvarargs}] = varargin{:};
-        [filename, basepath] = optargs{:};
+        [filename, basepath,delimiter] = optargs{:};
         if isempty(filename)
             filename = 'config/config.txt';
         end
